bb0b331b651d584f63b5ff5b30a8bb905a0ddfe7
tachiyomi <naishi1103@gmail.com>
2020-03-10 18:20:44 +0900

    update firebase todo

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,79 @@
 <template>
   <div id="app">
-    <h2>タスク</h2>
-    <div>
-      <input type="text" v-model="newTodoName">
-      <button type="submit" v-on:click="createTodo()">タスク作成</button>
-    </div>
+    <label>名前</label>
+    <input type="text" v-model="userName" />
+
+    <label>メッセージ</label>
+    <input type="text" v-model="message" />
+    <button @click="sendMessage">送信</button>
     <ul>
-      <li><button type="submit" v-on:click="showTodoType = 'all'">すべて</button></li>
-      <li><button type="submit" v-on:click="showTodoType = 'active'">未完タスク一覧</button></li>
-      <li><button type="submit" v-on:click="showTodoType = 'complete'">完了タスク一覧</button></li>
-    </ul>
-    <ul v-for="(todo, key) in filteredTodos">
-      <li><input class="toggle" type="checkbox" v-model="todo.isComplete" v-on:click="updateIsCompleteTodo(todo, key)">{{ todo.name }}</li>
-      <button type="submit" v-on:click="deleteTodo(key)">削除</button>
+      <li v-for="(value, key, index) in messageList" v-bind:key="index">
+        {{ value.user_name }}
+        <span style="margin-left:100px;">{{ value.message}}</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 import firebase from 'firebase'
+let messageRef
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      messageList: [],
+      userName: 'ユーザーA',
+      message: 'テストメッセージです'
+    };
+  },
   created: function() {
-    this.database = firebase.database();
-    this.todosRef = this.database.ref('todos');
+    // Your web app's Firebase configuration
+    // この設定情報はFirebaseのコンソールでプロジェクトの設定から取得して貼り付けてください。
+    var firebaseConfig = {
+          apiKey: "AIzaSyBs3EkI7xTbHdOpHlrgvpg_7uMapxcBPp0",
+          authDomain: "githubpages-todo-fb.firebaseapp.com",
+          databaseURL: "https://githubpages-todo-fb.firebaseio.com",
+          projectId: "githubpages-todo-fb",
+          storageBucket: "githubpages-todo-fb.appspot.com",
+          messagingSenderId: "866981490178",
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-    var _this = this;
-    this.todosRef.on('value', function(snapshot) {
-      _this.todos = snapshot.val(); // データに変化が起きたときに再取得する
+    const db = firebase.firestore()
+    messageRef = db.collection("chat_messages")
+
+    let messageList = this.messageList
+    messageRef.orderBy('created', 'desc').onSnapshot(function(qs) {
+      messageList.length = 0;
+      qs.forEach(result => {
+        messageList.push(result.data())
+      })
     });
   },
-  computed: {
-    filteredTodos: function () {
-      if (this.showTodoType == 'all') {
-        return this.todos;
-      } else {
-        var showIsComplete = false;
-        if (this.showTodoType == 'complete') { showIsComplete = true }
-        var filterTodos = {};
-        for (var key in this.todos) {
-          var todo = this.todos[key];
-          if (todo.isComplete == showIsComplete) { filterTodos[key] = todo; }
-        }
-        return filterTodos;
-      }
-    }
-  },
   methods: {
-    createTodo: function() {
-      if (this.newTodoName == "") { return; }
-      this.todosRef.push({
-        name: this.newTodoName,
-        isComplete: false,
-      })
-      this.newTodoName = "";
-    },
-    updateIsCompleteTodo: function (todo, key) {
-      todo.isComplete = !todo.isComplete
-      var updates = {};
-      updates['/todos/' + key] = todo;
-      this.database.ref().update(updates);
-    },
-    deleteTodo: function (key) {
-      this.database.ref('todos').child(key).remove();
-    },
-  },
-
-  data () {
-    return {
-      database: null,
-      todosRef: null,
-      newTodoName: '',
-      showTodoType: 'all',
-      todos: []
+    sendMessage: function() {
+      messageRef.add(
+        {
+          user_name: this.userName,
+          message: this.message,
+          created: new Date().getTime()
+        }
+      )
     }
   }
 }
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import firebase from 'firebase'
 
-Vue.config.productionTip = false
-
-var config = {
-        apiKey: "AIzaSyBs3EkI7xTbHdOpHlrgvpg_7uMapxcBPp0",
-        authDomain: "githubpages-todo-fb.firebaseapp.com",
-        databaseURL: "https://githubpages-todo-fb.firebaseio.com",
-        projectId: "githubpages-todo-fb",
-        storageBucket: "githubpages-todo-fb.appspot.com",
-        messagingSenderId: "866981490178",
-};
-firebase.initializeApp(config);
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
